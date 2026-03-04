7fc38742e340e8ae11878f9cf86a30364a6e6604
YKenanK-70-31 <keniened@gmail.com>
2026-03-04 12:11:20 +0100

    .

--- resources/views/meldingen/create.php
+++ resources/views/meldingen/create.php
@@ -1,67 +1,67 @@
-<?php require_once __DIR__.'/../../../config/config.php'; ?>
-<!doctype html>
-<html lang="nl">
+    <?php require_once __DIR__.'/../../../config/config.php'; ?>
+    <!doctype html>
+    <html lang="nl">
 
-<head>
-    <title>StoringApp / Meldingen / Nieuw</title>
-    <?php require_once __DIR__.'/../components/head.php'; ?>
-</head>
+    <head>
+        <title>StoringApp / Meldingen / Nieuw</title>
+        <?php require_once __DIR__.'/../components/head.php'; ?>
+    </head>
 
-<body>
+    <body>
 
-    <?php require_once __DIR__.'/../components/header.php'; ?>
+        <?php require_once __DIR__.'/../components/header.php'; ?>
 
-    <div class="container">
-        <h1>Nieuwe melding</h1>
+        <div class="container">
+            <h1>Nieuwe melding</h1>
 
-        <form action="<?php echo $base_url; ?>/app/Http/Controllers/meldingenController.php" method="POST">
+            <form action="<?php echo $base_url; ?>/app/Http/Controllers/meldingenController.php" method="POST">
 
-            <div class="form-group">
-                <label for="attractie">Naam attractie:</label>
-                <input type="text" name="attractie" id="attractie" class="form-input">
-            </div>
-            <div class="form-group">
-                <label for="type">Type</label>
-                <!-- hier komt een dropdown -->
-                 <form method="POST" action="meldingenController.php">
+                <div class="form-group">
+                    <label for="attractie">Naam attractie:</label>
+                    <input type="text" name="attractie" id="attractie" class="form-input">
+                </div>
+                <div class="form-group">
+                    <label for="type">Type</label>
+                    <!-- hier komt een dropdown -->
+                    <form method="POST" action="meldingenController.php">
 
-  <input type="text" name="attractie" placeholder="Attractie" required>
+    <input type="text" name="attractie" placeholder="Attractie" required>
 
-  <input type="text" name="capaciteit" placeholder="Capaciteit" required>
+    <input type="text" name="capaciteit" placeholder="Capaciteit" required>
 
-  <input type="text" name="melder" placeholder="Melder" required>
+    <input type="text" name="melder" placeholder="Melder" required>
 
-  <select name="type" required>
-    <option value="">-- Kies type --</option>
-    <option value="Technisch">Technisch</option>
-    <option value="Veiligheid">Veiligheid</option>
-    <option value="Overig">Overig</option>
-  </select>
+    <select name="type" required>
+        <option value="">-- Kies type --</option>
+        <option value="Technisch">Technisch</option>
+        <option value="Veiligheid">Veiligheid</option>
+        <option value="Overig">Overig</option>
+    </select>
 
-  <label>
-    <input type="checkbox" name="prioriteit" value="1"> Prioriteit
-  </label>
+    <label>
+        <input type="checkbox" name="prioriteit" value="1"> Prioriteit
+    </label>
 
-  <textarea name="overige_info" placeholder="Overige info"></textarea>
+    <textarea name="overige_info" placeholder="Overige info"></textarea>
 
-  <button type="submit">Opslaan</button>
+    <button type="submit">Opslaan</button>
 
-</form>
-            </div>
-            <div class="form-group">
-                <label for="capaciteit">Capaciteit p/uur:</label>
-                <input type="number" min="0" name="capaciteit" id="capaciteit" class="form-input">
-            </div>
-            <div class="form-group">
-                <label for="melder">Naam melder:</label>
-                <input type="text" name="melder" id="melder" class="form-input">
-            </div>
+    </form>
+                </div>
+                <div class="form-group">
+                    <label for="capaciteit">Capaciteit p/uur:</label>
+                    <input type="number" min="0" name="capaciteit" id="capaciteit" class="form-input">
+                </div>
+                <div class="form-group">
+                    <label for="melder">Naam melder:</label>
+                    <input type="text" name="melder" id="melder" class="form-input">
+                </div>
 
-            <input type="submit" value="Verstuur melding">
+                <input type="submit" value="Verstuur melding">
 
-        </form>
-    </div>
+            </form>
+        </div>
 
-</body>
+    </body>
 
-</html>
+    </html>
--- resources/views/meldingen/index.php
+++ resources/views/meldingen/index.php
@@ -1,10 +1,39 @@
-<?php require_once __DIR__.'/../../../config/config.php'; ?>
+<?php require_once __DIR__.'/../../../config/conn.php'; ?>
+
+<?php
+// SELECT-query uitvoeren
+$query = "SELECT * FROM meldingen";
+$statement = $conn->prepare($query);
+$statement->execute();
+$meldingen = $statement->fetchAll(PDO::FETCH_ASSOC);
+?>
+
 <!doctype html>
 <html lang="nl">
 
 <head>
     <title>StoringApp / Meldingen</title>
     <?php require_once __DIR__.'/../components/head.php'; ?>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        th {
+            background-color: #d3d3d3;
+            padding: 8px;
+            text-align: left;
+        }
+        td {
+            padding: 8px;
+        }
+        tr:nth-child(odd) {
+            background-color: #ebebeb;
+        }
+        tr:hover {
+            background-color: #d3d3d3;
+        }
+    </style>
 </head>
 
 <body>
@@ -15,13 +44,35 @@
         <h1>Meldingen</h1>
         <a href="create.php">Nieuwe melding &gt;</a>
 
-        <?php if(isset($_GET['msg']))
-        {
-            echo "<div class='msg'>" . $_GET['msg'] . "</div>";
-        } ?>
+        <?php if(isset($_GET['msg'])): ?>
+            <div class='msg'><?php echo htmlspecialchars($_GET['msg']); ?></div>
+        <?php endif; ?>
 
-        7<div style="height: 300px; background: #ededed; display: flex; justify-content: center; align-items: center; color: #666666;">(hier komen de storingsmeldingen)</div>
-        <div class="melidngen-overzicht">
+            
+        <div class="meldingen-overzicht">
+            <table border="1">
+                <tr>
+                    <th>ID</th>
+                    <th>Attractie</th>
+                    <th>Capaciteit</th>
+                    <th>Melder</th>
+                    <th>Type</th>
+                    <th>Prioriteit</th>
+                    <th>Overige info</th>
+                </tr>
+                <?php foreach ($meldingen as $melding): ?>
+                <tr>
+                    <td><?php echo htmlspecialchars($melding['id']); ?></td>
+                    <td><?php echo htmlspecialchars($melding['attractie']); ?></td>
+                    <td><?php echo htmlspecialchars($melding['capaciteit']); ?></td>
+                    <td><?php echo htmlspecialchars($melding['melder']); ?></td>
+                    <td><?php echo htmlspecialchars($melding['type']); ?></td>
+                    <td><?php echo $melding['prioriteit'] ? 'Ja' : 'Nee'; ?></td>
+                    <td><?php echo htmlspecialchars($melding['overige_info']); ?></td>
+                </tr>
+                <?php endforeach; ?>
+            </table>
+        </div>
 
     </div>
 
